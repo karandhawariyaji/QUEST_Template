98684f420002798adb364b0aef228eda1e686606
karandhawariyaji <karandhawariyaji@gmail.com>
2024-11-13 19:20:04 +0530

    Update backend.tf

--- Org_Template/terraform-google-cloud-folder/backend.tf
+++ Org_Template/terraform-google-cloud-folder/backend.tf
@@ -1,8 +1,8 @@
 #.................................... Terraform Backend ....................................#
 
-/*terraform {
+terraform {
     backend "gcs" {
     bucket  = "slz-poc" 
     #prefix  = "organization"
   }
-}*/
+}
