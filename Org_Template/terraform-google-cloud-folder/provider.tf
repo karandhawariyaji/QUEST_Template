8ec3356c0d02e99702aac66961c1db365ef03370
karandhawariyaji <karandhawariyaji@gmail.com>
2024-11-13 16:44:29 +0530

    Update provider.tf

--- Org_Template/terraform-google-cloud-folder/provider.tf
+++ Org_Template/terraform-google-cloud-folder/provider.tf
@@ -12,3 +12,16 @@ provider "google-beta" {
 terraform {
   required_version = ">= 1.3.0"
 }
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.0.0" # Specify the required version
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 4.0.0" # Specify the required version
+    }
+  }
+}
