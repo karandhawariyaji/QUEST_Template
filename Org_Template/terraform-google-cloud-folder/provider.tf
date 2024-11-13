6e29ce4893926cac1145df56f53b7506b34b44d9
karandhawariyaji <karandhawariyaji@gmail.com>
2024-11-13 16:51:43 +0530

    Update provider.tf

--- Org_Template/terraform-google-cloud-folder/provider.tf
+++ Org_Template/terraform-google-cloud-folder/provider.tf
@@ -2,11 +2,11 @@
 
 provider "google" {
     project         = var.project_id
-    access_token    = var.access_token
+    #access_token    = var.access_token
 }
 provider "google-beta" {
     project         = var.project_id
-    access_token    = var.access_token
+    #access_token    = var.access_token
 }
 
 terraform {
