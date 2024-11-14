945963a4cd913ac00ce0be2b805cecfaba7e410b
karandhawariyaji <karandhawariyaji@gmail.com>
2024-11-14 12:05:08 +0530

    Update folder.tf

--- Org_Template/terraform-google-cloud-folder/folder.tf
+++ Org_Template/terraform-google-cloud-folder/folder.tf
@@ -4,7 +4,7 @@ module "Level_2_BU_Folders" {
   for_each     = {  
     for k, v in try(var.Level_2_BU_Folders,{}): k => v if v.delete != true            
   }     
-  #source        = "git::https://github.com/Karandhawariyaji/QUEST_Module.git?ref=v0.0.3"
+  #source        = "git::https://github.com/Karandhawariyaji/QUEST_Module.git?ref=v0.0.3" 
   #source        =  "git::https://${secrets.token}@github.com/KarandhawariyaJI/QUEST_Module//Org_Module//terraform-google-cloud-folder"
   #source        =  "git::https://${var.git_token}@github.com/KarandhawariyaJI/QUEST_Module//Org_Module//terraform-google-cloud-folder"  
   source        =  "git::https://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@github.com/KarandhawariyaJI/QUEST_Module.git//Org_Module//terraform-google-cloud-folder?ref=v1.0.0"       
