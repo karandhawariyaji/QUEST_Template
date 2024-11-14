3e9b5dc232e3bc7f30d1dfe02822b8b6f4b19afa
karandhawariyaji <karandhawariyaji@gmail.com>
2024-11-14 14:29:55 +0530

    Update variables.tf

--- Org_Template/terraform-google-cloud-org-policy/variables.tf
+++ Org_Template/terraform-google-cloud-org-policy/variables.tf
@@ -7,3 +7,20 @@ variable "org_policy" {
    type = any
    sensitive = false
 }
+
+terraform {
+  required_version = ">= 1.3.0"
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.0.0" # Specify the required version
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 4.0.0" # Specify the required version
+    }
+  }
+}
