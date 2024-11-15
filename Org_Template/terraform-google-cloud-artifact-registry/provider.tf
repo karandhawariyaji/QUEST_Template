97d8fd8c7f1482cb4f5df26590e0df98b1a3c339
karandhawariyaji <karandhawariyaji@gmail.com>
2024-11-15 11:38:24 +0530

    Create provider.tf

---
+++ Org_Template/terraform-google-cloud-artifact-registry/provider.tf
@@ -0,0 +1,9 @@
+#..................................... Provider Block ......................................#
+provider "google" {
+    project         = var.project_id
+    #access_token    = var.access_token
+}
+provider "google-beta" {
+    project         = var.project_id
+    #access_token    = var.access_token
+}
